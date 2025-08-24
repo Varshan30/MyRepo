90bbb745c5309c47aa5800d73d44df1b89d135b8
Srivarshan <ssvarshan30@gmail.com>
2025-08-25 00:10:07 +0530

    Initial commit

---
+++ commit_animation.py
@@ -0,0 +1,25 @@
+import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Get commit dates
+log = subprocess.check_output(["git", "log", "--pretty=format:%ad", "--date=short"]).decode().splitlines()
+dates = pd.to_datetime(log)
+commits_per_day = dates.value_counts().sort_index()
+
+fig, ax = plt.subplots()
+
+def update(frame):
+    ax.clear()
+    ax.bar(commits_per_day.index[:frame], commits_per_day.values[:frame])
+    ax.set_title("Commits Over Time")
+    ax.tick_params(axis='x', rotation=45)
+
+ani = FuncAnimation(fig, update, frames=len(commits_per_day), repeat=False)
+
+# Save as MP4 (needs FFmpeg)
+ani.save("commit_animation.mp4", writer="ffmpeg", fps=10)
+
+# OR save as GIF (requires Pillow)
+# ani.save("commit_animation.gif", writer="pillow", fps=10)
